70782517a87984913996b8493f21dc0395a3e331
Student <Student@Train11-2-PC332.control.ad.sit.kmutt.ac.th>
2025-07-22 16:35:46 +0700

    kongpop

--- Class_2.py
+++ Class_2.py
@@ -3,14 +3,14 @@ print(km)
 if (int(km) < 5):
     print("ไม่ส่ง")
 elif (int(km) <= 5):
-    print("10")
+    print("ค่าขนส่ง 10")
 elif (int(km) <= 51):
-    print("15")
+    print("ค่าขนส่ง 15")
 elif (int(km) <= 101):
-    print("25")
+    print("ค่าขนส่ง 25")
 elif (int(km) <= 301):
-    print("35")
+    print("ค่าขนส่ง 35")
 elif (int(km) <= 500):
-    print("45")
+    print("ค่าขนส่ง 45")
 else:
     print
